f572042991e66975e097062f82aaafb771b91c02
chloros <chloros.lin@gmail.com>
2024-05-05 22:11:51 +0800

    set Routes

--- backend/controllers/goalControllers.js
+++ backend/controllers/goalControllers.js
@@ -1,22 +1,24 @@
-const getGoals = (req, res) => {
-    res.status(200).json({message: 'Get Goals'})
-}
+const asyncHandler = require('express-async-handler');
 
-const setGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
+    res.status(200).json({message: 'Get Goals'})
+})
+
+const setGoals = asyncHandler(async (req, res) => {
     if(!req.body.text){
         res.status(400)
         throw new Error('Please add a text field')
     }
     res.status(200).json({message: 'Set Goals'})
-}
+})
 
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
     res.status(200).json({message: `Update Goal ${req.params.id}`})
-}
+})
 
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
     res.status(200).json({message: `Delete Goal ${req.params.id}`})
-}
+})
 
 module.exports = {
     getGoals,
